906311127f56533a03e335814e1a3e71555fbd67
valentin bracchetti <valentin.bctt@gmail.com>
2024-12-18 14:32:00 +0100

    Configure Action Mailer for production with correct host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-    config.action_mailer.delivery_method = :smtp
+  # Other configurations...
+
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.example.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    domain: ENV['SMTP_DOMAIN'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
     enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: 'your-app.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 

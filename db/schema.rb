1b309c937a3b6ca05c5a3ed3a877bc4dba090b02
valentin bracchetti <valentin.bctt@gmail.com>
2024-12-18 11:49:41 +0100

    Update schema.rb after running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -39,15 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_18_101801) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "prestations", force: :cascade do |t|
-    t.string "titre"
-    t.text "description"
-    t.string "prix"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "rdvs", force: :cascade do |t|
     t.integer "prestation_id", null: false
     t.string "first_name"

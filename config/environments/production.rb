ad1ab98de2d064c0359642f1335d8a117d4e7fc4
valentin bracchetti <valentin.bctt@gmail.com>
2024-12-18 14:49:28 +0100

    Configure Action Mailer for production with correct host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,11 +5,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],
-    port: ENV['SMTP_PORT'],
-    domain: ENV['SMTP_DOMAIN'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'valentin.bctt@gmail.com',
+    password: 'qwkl cjqb sidy jcub',
     authentication: 'plain',
     enable_starttls_auto: true
   }

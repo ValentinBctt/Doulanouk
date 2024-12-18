0220af8cdaa143afa4534fa3d6f231d2261ddb74
valentin bracchetti <valentin.bctt@gmail.com>
2024-12-18 11:59:37 +0100

    Fix prestations table creation

--- db/migrate/20241217180332_create_prestations.rb
+++ db/migrate/20241217180332_create_prestations.rb
@@ -1,5 +1,8 @@
 class CreatePrestations < ActiveRecord::Migration[7.1]
   def change
+
+    drop_table :prestations, if_exists: true
+    
     create_table :prestations do |t|
       t.string :titre
       t.text :description

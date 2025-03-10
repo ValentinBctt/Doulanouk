34696dd9392d409ba072dd9ec2e5b474e3d0d32b
valentin bracchetti <valentin.bctt@gmail.com>
2025-03-10 14:23:43 +0100

    assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,5 @@ pin "application", preload: true
 pin "react", to: "https://cdn.skypack.dev/react"
 pin "react-dom", to: "https://cdn.skypack.dev/react-dom"
 pin_all_from "app/javascript/components", under: "components"
+pin "application", preload: true
+pin "controllers", to: "controllers.js"
